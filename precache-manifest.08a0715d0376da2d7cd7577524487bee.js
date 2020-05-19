029209f763fa70b3ce95c80d82235927a3b37750
Steven Tidd <steven.tidd@gmail.com>
2020-05-19 07:59:58 -0400

    Updates

---
+++ precache-manifest.08a0715d0376da2d7cd7577524487bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec593229c0170af1c3d3107f1c6054a",
+    "url": "/firebase-reactjs-testing/index.html"
+  },
+  {
+    "revision": "7a01b337c8eda33a0f4a",
+    "url": "/firebase-reactjs-testing/static/css/main.508a298c.chunk.css"
+  },
+  {
+    "revision": "498d62703df04c531f17",
+    "url": "/firebase-reactjs-testing/static/js/2.c2a36843.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/firebase-reactjs-testing/static/js/2.c2a36843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a01b337c8eda33a0f4a",
+    "url": "/firebase-reactjs-testing/static/js/main.a942e64d.chunk.js"
+  },
+  {
+    "revision": "a681b5754833d4e867bf",
+    "url": "/firebase-reactjs-testing/static/js/runtime-main.59113aaf.js"
+  }
+]);
